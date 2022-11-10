814172f0e953884b2b5c8abfba36443219847b84
pranavsharechat <pranavpaharia@sharechat.co>
2022-11-10 17:52:48 +0530

    added base  static classes

--- Source/CarHustle/CarHustleGameMode.cpp
+++ Source/CarHustle/CarHustleGameMode.cpp
@@ -3,9 +3,13 @@
 #include "CarHustleGameMode.h"
 #include "CarHustlePawn.h"
 #include "CarHustleHud.h"
+#include "CarGameSession.h"
+#include "CarPlayerState.h"
 
 ACarHustleGameMode::ACarHustleGameMode()
 {
 	DefaultPawnClass = ACarHustlePawn::StaticClass();
 	HUDClass = ACarHustleHud::StaticClass();
+	GameSessionClass = ACarGameSession::StaticClass();
+	PlayerStateClass = ACarPlayerState::StaticClass();
 }
