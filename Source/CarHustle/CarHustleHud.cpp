ab8d2ab7174bb3a7e87823ce7146beb66a5a3515
pranavsharechat <pranavpaharia@sharechat.co>
2022-11-10 17:51:51 +0530

    removed default HUD fonts

--- Source/CarHustle/CarHustleHud.cpp
+++ Source/CarHustle/CarHustleHud.cpp
@@ -19,8 +19,8 @@
 
 ACarHustleHud::ACarHustleHud()
 {
-	static ConstructorHelpers::FObjectFinder<UFont> Font(TEXT("/Engine/EngineFonts/RobotoDistanceField"));
-	HUDFont = Font.Object;
+	/*static ConstructorHelpers::FObjectFinder<UFont> Font(TEXT("/Engine/EngineFonts/RobotoDistanceField"));
+	HUDFont = Font.Object;*/
 }
 
 void ACarHustleHud::DrawHUD()
@@ -28,34 +28,34 @@ void ACarHustleHud::DrawHUD()
 	Super::DrawHUD();
 
 	// Calculate ratio from 720p
-	const float HUDXRatio = Canvas->SizeX / 1280.f;
-	const float HUDYRatio = Canvas->SizeY / 720.f;
-
-	bool bHMDDeviceActive = false;
-
-	// We dont want the onscreen hud when using a HMD device	
-#if HMD_MODULE_INCLUDED
-	bHMDDeviceActive = GEngine->IsStereoscopic3D();
-#endif // HMD_MODULE_INCLUDED
-	if( bHMDDeviceActive == false )
-	{
-		// Get our vehicle so we can check if we are in car. If we are we don't want onscreen HUD
-		ACarHustlePawn* Vehicle = Cast<ACarHustlePawn>(GetOwningPawn());
-		if ((Vehicle != nullptr) && (Vehicle->bInCarCameraActive == false))
-		{
-			FVector2D ScaleVec(HUDYRatio * 1.4f, HUDYRatio * 1.4f);
-
-			// Speed
-			FCanvasTextItem SpeedTextItem(FVector2D(HUDXRatio * 805.f, HUDYRatio * 455), Vehicle->SpeedDisplayString, HUDFont, FLinearColor::White);
-			SpeedTextItem.Scale = ScaleVec;
-			Canvas->DrawItem(SpeedTextItem);
-
-			// Gear
-			FCanvasTextItem GearTextItem(FVector2D(HUDXRatio * 805.f, HUDYRatio * 500.f), Vehicle->GearDisplayString, HUDFont, Vehicle->bInReverseGear == false ? Vehicle->GearDisplayColor : Vehicle->GearDisplayReverseColor);
-			GearTextItem.Scale = ScaleVec;
-			Canvas->DrawItem(GearTextItem);
-		}
-	}
+//	const float HUDXRatio = Canvas->SizeX / 1280.f;
+//	const float HUDYRatio = Canvas->SizeY / 720.f;
+//
+//	bool bHMDDeviceActive = false;
+//
+//	// We dont want the onscreen hud when using a HMD device	
+//#if HMD_MODULE_INCLUDED
+//	bHMDDeviceActive = GEngine->IsStereoscopic3D();
+//#endif // HMD_MODULE_INCLUDED
+//	if( bHMDDeviceActive == false )
+//	{
+//		// Get our vehicle so we can check if we are in car. If we are we don't want onscreen HUD
+//		ACarHustlePawn* Vehicle = Cast<ACarHustlePawn>(GetOwningPawn());
+//		if ((Vehicle != nullptr) && (Vehicle->bInCarCameraActive == false))
+//		{
+//			FVector2D ScaleVec(HUDYRatio * 1.4f, HUDYRatio * 1.4f);
+//
+//			// Speed
+//			FCanvasTextItem SpeedTextItem(FVector2D(HUDXRatio * 805.f, HUDYRatio * 455), Vehicle->SpeedDisplayString, HUDFont, FLinearColor::White);
+//			SpeedTextItem.Scale = ScaleVec;
+//			Canvas->DrawItem(SpeedTextItem);
+//
+//			// Gear
+//			FCanvasTextItem GearTextItem(FVector2D(HUDXRatio * 805.f, HUDYRatio * 500.f), Vehicle->GearDisplayString, HUDFont, Vehicle->bInReverseGear == false ? Vehicle->GearDisplayColor : Vehicle->GearDisplayReverseColor);
+//			GearTextItem.Scale = ScaleVec;
+//			Canvas->DrawItem(GearTextItem);
+//		}
+//	}
 }
 
 #undef LOCTEXT_NAMESPACE
