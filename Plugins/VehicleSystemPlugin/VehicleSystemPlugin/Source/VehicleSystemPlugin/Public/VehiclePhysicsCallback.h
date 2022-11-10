20ca0d9a71059dabcf2709ec09c20c29d30385f2
pranavsharechat <pranavpaharia@sharechat.co>
2022-11-10 17:50:55 +0530

    Added vehicle system plugin

---
+++ Plugins/VehicleSystemPlugin/VehicleSystemPlugin/Source/VehicleSystemPlugin/Public/VehiclePhysicsCallback.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "PhysicsProxy/SingleParticlePhysicsProxy.h"
+
+struct FVehiclePhysicsPhysicsInput : public Chaos::FSimCallbackInput
+{
+	TWeakObjectPtr<APawn> VehicleActor; //Has to be APawn to avoid circular references
+	FBodyInstance* VehicleMeshBodyInstance;
+
+	void Reset() //Required
+	{
+		VehicleActor = nullptr;
+		VehicleMeshBodyInstance = nullptr;
+	}
+}; 
+struct FVehiclePhysicsPhysicsOutput : public Chaos::FSimCallbackOutput
+{
+	float ChaosDeltaTime = 0.0f;
+	
+	void Reset() //Required
+	{
+		ChaosDeltaTime = 0.0f;
+	}
+};
+
+class FVehiclePhysicsCallback : public Chaos::TSimCallbackObject<FVehiclePhysicsPhysicsInput, FVehiclePhysicsPhysicsOutput>
+{
+public:
+	FSingleParticlePhysicsProxy* VehicleMesh;
+	TArray<FSingleParticlePhysicsProxy*> WheelMeshes;
+private:
+	virtual void OnPreSimulate_Internal() override;
+	virtual void OnContactModification_Internal(Chaos::FCollisionContactModifier& Modifier) override;
+};
